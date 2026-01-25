4d35959cbf657aa1940bd5afd19ab13340bf36f2
Aarambha Timalsena <timalsenaaarambha75@gmail.com>
2026-01-25 11:44:04 +0545

    added package for secuirty

---
+++ frontend/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "localhost",
+    port: 5173,
+    https: {
+      key: fs.readFileSync(path.resolve("./ssl/key.pem")),
+      cert: fs.readFileSync(path.resolve("./ssl/cert.pem")),
+    },
+    proxy: {
+      "/api": {
+        target: "https://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
